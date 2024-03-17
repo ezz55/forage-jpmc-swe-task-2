e82431d6477b070d62fe4b60dd5cc6037c95f024
ezz55 <ezzaldin55@gmail.com>
2024-03-17 22:59:38 +0000

    Code Update

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,44 +1,59 @@
-import React, { Component } from 'react';
-import { Table } from '@finos/perspective';
-import { ServerRespond } from './DataStreamer';
-import './Graph.css';
+import React, { Component } from "react";
+import { Table } from "@finos/perspective";
+import { ServerRespond } from "./DataStreamer";
+import "./Graph.css";
 
 /**
  * Props declaration for <Graph />
  */
 interface IProps {
-  data: ServerRespond[],
+  data: ServerRespond[];
 }
 
 /**
  * Perspective library adds load to HTMLElement prototype.
  * This interface acts as a wrapper for Typescript compiler.
  */
-interface PerspectiveViewerElement {
-  load: (table: Table) => void,
+interface PerspectiveViewerElement extends HTMLElement {
+  load: (table: Table) => void;
+}
+
+interface IState {
+  lastUpdateTimestamp: number;
 }
 
 /**
  * React component that renders Perspective based on data
  * parsed from its parent through data property.
  */
-class Graph extends Component<IProps, {}> {
+class Graph extends Component<IProps, IState> {
+  // Update the state type here
   // Perspective table
   table: Table | undefined;
 
+  constructor(props: IProps) {
+    super(props);
+    // Initialise the state
+    this.state = {
+      lastUpdateTimestamp: 0,
+    };
+  }
+
   render() {
-    return React.createElement('perspective-viewer');
+    return React.createElement("perspective-viewer");
   }
 
   componentDidMount() {
     // Get element to attach the table from the DOM.
-    const elem: PerspectiveViewerElement = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+    const elem = (document.getElementsByTagName(
+      "perspective-viewer"
+    )[0] as unknown) as PerspectiveViewerElement;
 
     const schema = {
-      stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
-      timestamp: 'date',
+      stock: "string",
+      top_ask_price: "float",
+      top_bid_price: "float",
+      timestamp: "date",
     };
 
     if (window.perspective && window.perspective.worker()) {
@@ -46,26 +61,51 @@ class Graph extends Component<IProps, {}> {
     }
     if (this.table) {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
-
-      // Add more Perspective configurations here.
+      elem.setAttribute("view", "y_line");
+      elem.setAttribute("column-pivots", '["stock"]');
+      elem.setAttribute("row-pivots", '["timestamp"]');
+      elem.setAttribute("columns", '["top_ask_price"]');
+      elem.setAttribute(
+        "aggregates",
+        `
+        {"stock" : "distinct count",
+        "top_ask_price" : "avg",
+        "top_bid_price": "avg" ,
+        "timestamp" : "distinct count" }`
+      );
       elem.load(this.table);
     }
   }
 
-  componentDidUpdate() {
-    // Everytime the data props is updated, insert the data into Perspective table
+  componentDidUpdate(prevProps: IProps) {
+    // Only update the table with new data.
     if (this.table) {
-      // As part of the task, you need to fix the way we update the data props to
-      // avoid inserting duplicated entries into Perspective table again.
-      this.table.update(this.props.data.map((el: any) => {
-        // Format the data from ServerRespond to the schema
-        return {
-          stock: el.stock,
-          top_ask_price: el.top_ask && el.top_ask.price || 0,
-          top_bid_price: el.top_bid && el.top_bid.price || 0,
-          timestamp: el.timestamp,
-        };
-      }));
+      const newData = this.props.data.filter((data) => {
+        // Convert timestamp to a number for comparison
+        const dataTimestamp = new Date(data.timestamp).getTime();
+        return dataTimestamp > this.state.lastUpdateTimestamp;
+      });
+
+      if (newData.length > 0) {
+        // Update the table with new data
+        this.table.update(
+          newData.map((el: any) => {
+            return {
+              stock: el.stock,
+              top_ask_price: (el.top_ask && el.top_ask.price) || 0,
+              top_bid_price: (el.top_bid && el.top_bid.price) || 0,
+              timestamp: el.timestamp,
+            };
+          })
+        );
+
+        // Update the lastUpdateTimestamp in the state
+        this.setState({
+          lastUpdateTimestamp: new Date(
+            newData[newData.length - 1].timestamp
+          ).getTime(),
+        });
+      }
     }
   }
 }
